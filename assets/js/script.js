ae3c04ec600c9dfe8b26400e0da829626c9848ed
Azwar-as <azwaranshari08@gmail.com>
2025-04-28 15:57:45 +0700

    cut contact Back-END

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const inpName = document.getElementById("inputName");
+const inpEmail = document.getElementById("inputEmail");
+const inpMessage = document.getElementById("inputMessage");
+const btnSend = document.getElementById("btnSend");
